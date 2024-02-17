479dc50a59f8d1e93253616eec83c7ad9c429cb6
@Bett <126676083+LlaryBett@users.noreply.github.com>
2024-02-17 13:10:27 +0300

    Add files via upload

--- Myproject.java
+++ Myproject.java
@@ -2,31 +2,50 @@ import java.util.Scanner;
 
 public class Myproject {
 
-	public static void main(String[] args) 
-	{
-		//create a scanner object to read input from the user
-		Scanner input = new Scanner (System.in);
-		//prompt the user to input first number
-		System.out.print("Input the first number:");
-		
-		//read and store the first number
-		int firstNumber=input.nextInt();
-		
-		//prompt the user to input the second number
-		System.out.print("Input the second number:");
-		//read and store the second number
-		int secondNumber=input.nextInt();
-		//calculate the sum of the two numbers
-	
-		 int sum = calculateSum(firstNumber, secondNumber);
-		//display the sum of the two numbers
-		System.out.println("The sum of" + firstNumber + "and" + secondNumber +"is"	+ sum);
-				
+
+	 public static void main(String[] args) {
+	  // Declare variables to store two binary numbers, an index, and a remainder
+	  long binary1, binary2;
+	  int i = 0, remainder = 0;
+	  
+	  // Create an array to store the sum of binary digits
+	  int[] sum = new int[20];
+	  
+	  // Create a Scanner object to read input from the user
+	  Scanner in = new Scanner(System.in);
+
+	  // Prompt the user to input the first binary number
+	  System.out.print("Input first binary number: ");
+	  binary1 = in.nextLong();
+	  
+	  // Prompt the user to input the second binary number
+	  System.out.print("Input second binary number: ");
+	  binary2 = in.nextLong();
+
+	  // Perform binary addition while there are digits in the binary numbers
+	  while (binary1 != 0 || binary2 != 0) 
+	  {
+	   // Calculate the sum of binary digits and update the remainder
+	   sum[i++] = (int)((binary1 % 10 + binary2 % 10 + remainder) % 2);
+	   remainder = (int)((binary1 % 10 + binary2 % 10 + remainder) / 2);
+	   binary1 = binary1 / 10;
+	   binary2 = binary2 / 10;
+	  }
+	  
+	  // If there is a remaining carry, add it to the sum
+	  if (remainder != 0) {
+	   sum[i++] = remainder;
+	  }
+	  
+	  // Decrement the index to prepare for printing
+	  --i;
+	  
+	  // Display the sum of the two binary numbers
+	  System.out.print("Sum of two binary numbers: ");
+	  while (i >= 0) {
+	   System.out.print(sum[i--]);
+	  }
+	  
+	  System.out.print("\n");  
+	 }
 	}
-
-	public static int calculateSum(int a, int b) {
-
-		return a+b ;
-	}
-
-}
